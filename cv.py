263b71ccf8446b4eee89d15410af7ef8abd6e7f1
Jun Saldivar <junsaldivar@Juns-MacBook-Air-2.local>
2026-03-06 06:44:10 +0800

    final output

--- cv.py
+++ cv.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hand.HandDetector()
 drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
+index_y=0
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
@@ -20,11 +21,20 @@ while True:
                 if id == 8:
                     x = int(landmark.x*frame_width
                     y = int(landmark.y*frame_height
-                    print(x, y)
                     if id == 8:
                         cv2.circle(img=frame, center=*x, y), radius = 10, color = (0, 255, 255))
                         index_x = screen_width/frame_width*x
                         index_y = screen_height/frame_height*y
                         pyautogui.moveTo(index_x, index_y)
+                    if id == 4:
+                        cv2.circle(img=frame, center=*x, y), radius = 10, color = (0, 255, 255))
+                        thumb_x = screen_width/frame_width*x
+                        thumb_y = screen_height/frame_height*y
+                    print("outside", abs(index_y - thumb_y))
+                    if abs(index_y - thumb_y) <20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
+                        print("click")
+
     cv2.imshow('Virtual_mouse', frame)
     cv2.waitKey(1)
