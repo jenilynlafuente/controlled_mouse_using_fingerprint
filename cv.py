460351b4cacc8c1b7892069f2f7c6bde1713cd57
Jun Saldivar <junsaldivar@Juns-MacBook-Air-2.local>
2026-03-06 05:59:59 +0800

    hand detector

--- cv.py
+++ cv.py
@@ -1,6 +1,12 @@
 import cv2
+import mediapipe as mp
 cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hand.HandDetector()
 while True:
     _, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.detect(rgb_frame)
+    hand = output.multi_hand_landmarks
+    print(hand)
     cv2.imshow('Virtual_mouse', frame)
     cv2.waitKey(1)
